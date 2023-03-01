b56e45db84cb2320c5ca5c08a19df98a529ea3e9
yugo <116182604+yuri-gordienko@users.noreply.github.com>
2023-03-01 15:17:00 +0200

    Delete hw_1_compilation directory

--- hw_1_compilation/level_1/package/ua/com/alevel/GoodDay.java
+++
@@ -1,7 +0,0 @@
-class GoodDay {
-
-    public static void main(String[] args) {
-        Words print = new Words();
-        print.printMessage("Good morning");
-    }
-}
--- hw_1_compilation/level_1/separate_packages/ua/com/alevel/test/Words.java
+++
@@ -1,9 +0,0 @@
-package ua.com.alevel.test;
-
-public class Words {
-
-    public void printMessage(String sms) {
-        System.out.println("this" + " " + sms);
-    }
-}
-
