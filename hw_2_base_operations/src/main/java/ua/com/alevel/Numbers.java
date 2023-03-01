f37d9ef733b8e8977c2bb8a7aed2bbf908ce4516
yugo <gordienko.yuri@gmail.com>
2023-03-01 13:52:20 +0200

    hw_2_base_operations

--- hw_2_base_operations/src/main/java/ua/com/alevel/Numbers.java
+++ hw_2_base_operations/src/main/java/ua/com/alevel/Numbers.java
@@ -2,11 +2,11 @@ package ua.com.alevel;
 
 public class Numbers {
     public int getNumbers(String count) {
-        char[] charsNum = count.toCharArray();
+        char[] charsNum = count.toCharArray();            // создаю из строки массив чаров
         int sum = 0;
-        for (char c : charsNum) {
-            if (c <= 57 && c >= 48) {
-                sum = sum + Character.getNumericValue(c);
+        for (char c : charsNum) {                         // пробежался по массиву
+            if (c <= 57 && c >= 48) {                     // если в введенной строке были цифры согласно диапазону (ascii),
+                sum = sum + Character.getNumericValue(c); // получаю числовое значение, связанное с указанным символом и суммирую значения данных символов
             }
         }
         return (sum);
--- hw_2_base_operations/src/main/java/ua/com/alevel/School.java
+++ hw_2_base_operations/src/main/java/ua/com/alevel/School.java
@@ -1,9 +1,12 @@
 package ua.com.alevel;
 
 import java.text.DecimalFormat;
+
 public class School {
+
     public void lesson(int ln) {
-        DecimalFormat dF = new DecimalFormat("#.###");
+
+        DecimalFormat dF = new DecimalFormat("#.###"); // форматирую числовое значение времени, добавляю "0" в минутах
 
         int h = 60;             // 1 hour = 60 min
         int l = 45;             // lesson time 45 min
@@ -17,6 +20,7 @@ public class School {
         int elm = (s + (e * b1) + ((e / 2) * ((b1 + b2) / 2)) + (ln * l)) % h;
         String str = Integer.toString(elm);
 
-        System.out.println("End of lesson # " + ln + " is: " + new DecimalFormat("00.###").format(elh) + ":" + new DecimalFormat("00.###").format(elm));
+        System.out.println("End of lesson # " + ln + " is: " + new DecimalFormat("00.###").format(elh) + ":" +
+                                                               new DecimalFormat("00.###").format(elm));
     }
 }
