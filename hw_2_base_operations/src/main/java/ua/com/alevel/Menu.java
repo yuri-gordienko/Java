8ca919a3678b4d403470a273f1d28bdbfecba69c
yugo <gordienko.yuri@gmail.com>
2023-03-01 16:35:26 +0200

    hw_2_base_operations

---
+++ hw_2_base_operations/src/main/java/ua/com/alevel/Menu.java
@@ -0,0 +1,58 @@
+package ua.com.alevel;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Menu {
+
+    public void start() throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String select;
+        menu();
+        while ((select = bf.readLine()) != null) {
+            variants(bf, select);
+        }
+    }
+    private void menu() {
+        System.out.println("_________________________________________________");
+        System.out.println("- 'Sum of numbers', please enter  1");
+        System.out.println("- 'Alphabet', please enter        2");
+        System.out.println("- 'Lessons', please enter         3");
+        System.out.println("- 'EXIT', please enter            0");
+    }
+    private void variants(BufferedReader reader, String select) throws IOException {
+        switch (select) {
+            case "1" -> numbers(reader);
+            case "2" -> words(reader);
+            case "3" -> lessons(reader);
+            case "0" -> System.exit(0);
+        }
+        menu();
+    }
+    private void numbers(BufferedReader bf) throws IOException {
+        System.out.println("You can Enter different symbols and get their sum:");
+        Numbers num = new Numbers();
+        System.out.println("symbol's sum is:" + " " + num.getNumbers(bf.readLine()));
+    }
+    private void words(BufferedReader bf) throws IOException {
+        System.out.println("You can Enter different symbols and get its in order alphabet with quantity of each other:");
+        String l = bf.readLine();
+        char[] letters = l.toCharArray();
+        Arrays.sort(letters);
+        String line = String.copyValueOf(letters);
+        for (int i = 0; i < letters.length; i++) {
+            if (Character.isLetter(letters[i])) {
+                System.out.println(letters[i] + " - " + (line.lastIndexOf(letters[i]) - line.indexOf(letters[i]) + 1));
+                i += (line.lastIndexOf(letters[i]) - line.indexOf(letters[i]));
+            }
+        }
+    }
+    private void lessons(BufferedReader bf) throws IOException {
+        System.out.println("You can Enter the lesson's number from 1 - 10, which you want to know about end:");
+        School el = new School();
+        el.lesson(Integer.parseInt(bf.readLine()));
+    }
+
+}
---
+++ hw_2_base_operations/src/main/java/ua/com/alevel/School.java
@@ -0,0 +1,27 @@
+package ua.com.alevel;
+
+import java.text.DecimalFormat;
+
+public class School {
+
+    public void lesson(int ln) {
+
+        DecimalFormat dF = new DecimalFormat("#.###");  // форматирую числовое значение времени, добавляю "0" в минутах
+
+        int h = 60;             // 1 hour = 60 min
+        int l = 45;             // lesson time 45 min
+        int b1 = 5;             // break of even lessons
+        int b2 = 15;            // break of not even lessons
+        int s = h * 9;          // start of lesson 1
+        int e = ln - 1;          // number of lesson
+
+        int elh = (s + (e * b1) + ((e / 2) * ((b1 + b2) / 2)) + (ln * l)) / h;
+        String sto = Integer.toString(elh);
+        int elm = (s + (e * b1) + ((e / 2) * ((b1 + b2) / 2)) + (ln * l)) % h;
+        String str = Integer.toString(elm);
+
+        System.out.println("End of lesson # " + ln + " is: " + new DecimalFormat("00.###").format(elh) + ":" +
+                                                               new DecimalFormat("00.###").format(elm));
+    }
+
+}
