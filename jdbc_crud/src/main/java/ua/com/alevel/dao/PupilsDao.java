5c16b61320b15b20026bc20febc08c51e9baafce
yugo <116182604+yuri-gordienko@users.noreply.github.com>
2023-04-23 21:54:17 +0300

    Delete jdbc_crud directory

--- jdbc_crud/src/main/java/ua/com/alevel/dao/PupilsDao.java
+++
@@ -1,8 +0,0 @@
-package ua.com.alevel.dao;
-
-import ua.com.alevel.entity.Pupils;
-
-public interface PupilsDao extends BaseDao<Pupils> {
-
-    boolean existsByFirstNameOrLastName(String firstName, String lastName);
-}
