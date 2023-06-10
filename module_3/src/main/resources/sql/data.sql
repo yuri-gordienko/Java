4494f11f13e2cdfc229f544ce0a98c94bf324d22
yugo <gordienko.yuri@gmail.com>
2023-06-10 11:02:04 +0300

    add feedback

---
+++ module_3/src/main/resources/sql/data.sql
@@ -0,0 +1,16 @@
+insert into clients values (1, 'bruce@mail.com', 'Bruce Lee');
+insert into clients values (2, 'norris@mail.com', 'Chuck Norris');
+insert into clients values (3, 'bruce@mail.com', 'Jean Claude Van Damme');
+insert into clients values (4, 'sly@mail.com', 'Sylvester Stallone');
+
+insert into accounts values (1, 'IBAN UA', 111111111, 200000, 1);
+insert into accounts values (2, 'IBAN UA', 111111112, 300000, 1);
+
+insert into accounts values (3, 'IBAN UA', 111111113, 400000, 2);
+
+insert into accounts values (4, 'IBAN UA', 111111114, 100000, 3);
+insert into accounts values (5, 'IBAN UA', 111111115, 500000, 3);
+
+insert into accounts values (6, 'IBAN UA', 111111116, 100000, 4);
+insert into accounts values (7, 'IBAN UA', 111111117, 500000, 4);
+insert into accounts values (8, 'IBAN UA', 111111118, 300000, 4);
