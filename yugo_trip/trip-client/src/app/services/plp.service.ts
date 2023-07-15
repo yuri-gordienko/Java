2f064bd9e8b7fabd38b44f398171b264b41fe683
yugo <gordienko.yuri@gmail.com>
2023-07-15 16:18:02 +0300

    updated

--- yugo_trip/trip-client/src/app/services/plp.service.ts
+++ yugo_trip/trip-client/src/app/services/plp.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map, Observable } from "rxjs";
+
 import { DataContainer } from "../models/data.container";
-// import { appSettings } from "../app.const";
+import { appSettings } from "../app.const";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlpService {
 
-  constructor(private _http: HttpClient) {
-  }
+  constructor(private _http: HttpClient) { }
 
   loadTours(): Observable<Object> {
-    return this._http.get('http://localhost:8080/tours/plp')
-     .pipe(
-        map(res => {
-        const data: DataContainer = res as DataContainer;
-        return data.data;
-        })
-     );
-  }
+    return this._http.get(appSettings.apiOpen + '/tours/plp')
+          .pipe(
+            map(res => {
+              const data: DataContainer = res as DataContainer;
+              return data.data;
+            })
+          );
+      }
 }
