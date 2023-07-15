a71ebf2e9e8e13f5d5839cfa34ddacc431fcc411
yugo <gordienko.yuri@gmail.com>
2023-07-15 17:56:03 +0300

    updated

--- yugo_trip/trip-client/src/app/pages/plp/plp.component.ts
+++ yugo_trip/trip-client/src/app/pages/plp/plp.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
 import { AsyncPipe, JsonPipe, NgForOf, NgIf } from "@angular/common";
+import { Observable } from "rxjs";
+
 import { PlpService } from "../../services/plp.service";
 import { TourPlpModel } from "../../models/tour-plp.model";
 
@@ -9,18 +10,23 @@ import { TourPlpModel } from "../../models/tour-plp.model";
   selector: 'app-plp',
   standalone: true,
   templateUrl: './plp.component.html',
-  styleUrls: ['./plp.component.scss'],
-  imports: [JsonPipe, AsyncPipe, NgIf, NgForOf]
+  imports: [
+    JsonPipe,
+    AsyncPipe,
+    NgIf,
+    NgForOf
+  ],
+  styleUrls: ['./plp.component.scss']
 })
 export class PlpComponent implements OnInit {
 
-plpList$: Observable<TourPlpModel[]> = this._plpService.loadTours() as Observable<TourPlpModel[]>;
+  plpList$: Observable<TourPlpModel[]> = this._plpService.loadTours() as Observable<TourPlpModel[]>;
 
   constructor(private _plpService: PlpService, private _router: Router) { }
 
   ngOnInit(): void { }
 
   redirectToPdp(tourId: number): void {
-      this._router.navigateByUrl('/pdp/' + tourId);
-    }
+    this._router.navigateByUrl('/pdp/' + tourId);
+  }
 }
