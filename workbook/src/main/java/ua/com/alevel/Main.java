b98659c120412fec8c270fdc9d3d831d583743e2
yugo <gordienko.yuri@gmail.com>
2023-08-25 18:32:37 +0300

    updated

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -4,50 +4,14 @@ public class Main {
 
     public static void main(String[] args){
 
-//        int numbers = count(2, 4, 3 );
-
-//        String s = "Java";
-//        s.concat(" SE 6");
-//        s.replace('6', '7');
-//        System.out.print(s);
-//
-//        int a[] = {1, 2, 053, 4};
-//        int b [][] = {{1, 2, 4}, {2, 2, 1}, {0, 43, 2}};
-//        System.out.println(a[3] == b[0][2]);
-//        System.out.println(" " + (a[2] == b[2][1]));
-
-//        String s = "Java";
-//        s.concat(" SE 6");
-//        s.toLowerCase();
-//        System.out.print(s);
-        // How many?
-
-//        int i = 10;
-//        int n = ++i % 5;
-//        System.out.println(i + " " + n);
-
-
-
+        int numbers = count(2, 4, 3 );
     }
 
-//    public static int count(int a, int b, int c) {
-//        int result = a + b + c;
-//        System.out.println("sum = " + result);
-//        return result;
-//    }
-
-
-//    public static void main(String[] args) {
-
-//    public boolean tru(){
-//        int a = 0;
-//        if(a == 10){
-//        }
-//        return true;
-//    }
-
-//    }
-
+    public static int count(int a, int b, int c) {
+        int result = a + b + c;
+        System.out.println("sum = " + result);
+        return result;
+    }
 }
 
 
