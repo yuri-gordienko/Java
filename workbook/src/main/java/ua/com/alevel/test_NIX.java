7371a7a8f399e745f88cf6f1ef3b0d05579a4fbf
yugo <gordienko.yuri@gmail.com>
2023-08-25 18:32:58 +0300

    tests

---
+++ workbook/src/main/java/ua/com/alevel/test_NIX.java
@@ -0,0 +1,56 @@
+package ua.com.alevel;
+
+public class test_NIX {
+
+    public static void main(String[] args){
+
+//        String s = "Java";
+//        s.concat(" SE 6");
+//        s.replace('6', '7');
+//        System.out.print(s);
+//
+//        int a[] = {1, 2, 053, 4};
+//        int b [][] = {{1, 2, 4}, {2, 2, 1}, {0, 43, 2}};
+//        System.out.println(a[3] == b[0][2]);
+//        System.out.println(" " + (a[2] == b[2][1]));
+
+//        String s = "Java";
+//        s.concat(" SE 6");
+//        s.toLowerCase();
+//        System.out.print(s);
+        // How many?
+
+//        int i = 10;
+//        int n = ++i % 5;
+//        System.out.println(i + " " + n);
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
