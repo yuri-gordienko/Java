7046843b265309facba3d80d8cf7182e03c5e771
yugo <gordienko.yuri@gmail.com>
2023-08-25 16:07:12 +0300

    postgres.sql

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -3,13 +3,36 @@ package ua.com.alevel;
 public class Main {
 
     public static void main(String[] args){
-        int numbers = count(2, 4, 3 );
+
+//        int numbers = count(2, 4, 3 );
+
+//        String s = "Java";
+//        s.concat(" SE 6");
+//        s.replace('6', '7');
+//        System.out.print(s);
+//
+//        int a[] = {1, 2, 053, 4};
+//        int b [][] = {{1, 2, 4}, {2, 2, 1}, {0, 43, 2}};
+//        System.out.println(a[3] == b[0][2]);
+//        System.out.println(" " + (a[2] == b[2][1]));
+
+//        String s = "Java";
+//        s.concat(" SE 6");
+//        s.toLowerCase();
+//        System.out.print(s);
+        // How many?
+
+//        int i = 10;
+//        int n = ++i % 5;
+//        System.out.println(i + " " + n);
+
+
     }
 
-    public static int count(int a, int b, int c) {
-        int result = a + b + c;
-        System.out.println("sum = " + result);
-        return result;
+//    public static int count(int a, int b, int c) {
+//        int result = a + b + c;
+//        System.out.println("sum = " + result);
+//        return result;
 //    }
 
 
@@ -22,7 +45,8 @@ public class Main {
 //        return true;
 //    }
 
-    }
+//    }
+
 }
 
 
