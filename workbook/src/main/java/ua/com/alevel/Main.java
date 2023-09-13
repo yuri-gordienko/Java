a8db4efcd9ed069abba8819e12b3912f815bd551
yugo <gordienko.yuri@gmail.com>
2023-09-13 13:43:00 +0300

    updated

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -22,19 +22,15 @@ public class Main {
             System.out.println("start = " + Arrays.toString(values));
 
             String[] res = new String[values.length];
-            int index = 0;
-            for (boolean val : values) {
-                if (val == true){
-                    return new String[]{"Yes"};
+            for (int i = 0; i < values.length; i++) {
+                if (values[i]){
+                    res[i] ="Yes";
                 } else {
-                    return new String[]{"No"};
+                    res[i] ="No";
                 }
-//                res[index] = String.valueOf(val);
-//                index++;
             }
-            System.out.println(res);
-
-            return null;
+            System.out.println(Arrays.toString(res));
+            return res;
 }
 
 
