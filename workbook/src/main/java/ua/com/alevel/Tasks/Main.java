c52012b7ad2e5fce2914403d12321c646aed0eb2
yugo <gordienko.yuri@gmail.com>
2023-09-21 12:06:47 +0300

    tests

--- workbook/src/main/java/ua/com/alevel/Tasks/Main.java
+++ workbook/src/main/java/ua/com/alevel/Tasks/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
     public static void main(String[] args){
 
+        int rait = reviewCode(10);
+        System.out.println(rait);
 
     }
+
+    public static int reviewCode(int maxAttempts) {
+        int solutionAttempts = 0;
+
+        do {
+            solutionAttempts++;
+        } while (solutionAttempts < maxAttempts);
+        return solutionAttempts;
+    }
+
 
 }
