e3623b6e70233e50a449085b13635aec025efcb0
yugo <gordienko.yuri@gmail.com>
2023-09-21 09:03:02 +0300

    updated

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -4,8 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
+        calculateTaxes(10000);
 
+    }
 
+    public static double calculateTaxes(double income) {
+        double tax = 0;
+        if (income <= 1000){
+            tax = income * 0.02;
+        } else if (income > 1000 && income <= 10000) {
+            tax = income * 0.03;
+        } else if (income > 10000) {
+            tax = income * 0.05;
+        }
+        System.out.println(tax);
+        return tax;
     }
 
 
