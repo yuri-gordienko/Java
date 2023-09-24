9d26c53eb2ef9cbd9c45be7f307f887ab22762de
yugo <gordienko.yuri@gmail.com>
2023-09-24 11:15:26 +0300

    tests

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -2,12 +2,23 @@ package ua.com.alevel;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+        int i = a.length - 1;
+        while (i > 0) {
+            System.out.println(a[i]);
+            i--;
+        }
     }
 
 
 }
+
+
+
+
 
 
 
