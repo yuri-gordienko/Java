cc5c9c849006af77b79199b1a28d6aa7504126f4
yugo <gordienko.yuri@gmail.com>
2023-10-13 12:45:23 +0300

    updated

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -1,21 +1,48 @@
 package ua.com.alevel;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int j = 0;
-        while (j <10){
-            System.out.println(j);
-            j++;
-            if (j == 5){
-                break;
+        String url = "https://jobs.dou.ua/vacancies/?category=Java";
+
+        try {
+            // Отримання вмісту веб-сторінки
+            Document document = Jsoup.connect(url).get();
+
+            // Вибір елементів, які містять вакансії (припустимо, що вони розміщені в елементах div з певним класом)
+            Elements vacancyElements = document.select("div.l-items#vacancyListId");
+
+            // Перегляд і виведення вакансій
+            for (Element vacancyElement : vacancyElements) {
+                // Отримання тексту або атрибутів для вакансії, наприклад:
+                String title = vacancyElement.select("h2").text();
+                String company = vacancyElement.select("p.company").text();
+                String location = vacancyElement.select("p.location").text();
+
+                System.out.println("Назва: " + title);
+                System.out.println("Компанія: " + company);
+                System.out.println("Місцезнаходження: " + location);
+                System.out.println();
             }
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
+
+
+
     }
+
 
 
 }
