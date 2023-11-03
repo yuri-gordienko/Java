36309118332633b7d5550f4fe112e30f4d2f52e2
yugo <gordienko.yuri@gmail.com>
2023-11-03 14:30:48 +0200

    updated

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -1,12 +1,35 @@
 package ua.com.alevel;
 
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        string("123456", "123456");
+//        String[] ingredients = {"dough", "cheese", "sauce", "dough"};
+//        doughIsEnough(ingredients);
+//        String input = "Hello, world!";
+//        string(input);
+
+
 
     }
+
+//    public static String string(String input) {
+        public static boolean string(String firstString, String secondString) {
+            if (firstString == null && secondString == null){
+                return true;
+            }
+            if (firstString.equals(secondString)){
+                return true;
+            }
+            return false;
+        }
+//    }
+
+
 
 
 }
