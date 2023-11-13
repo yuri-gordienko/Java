94179c58d54096e07eacd38e7f5f958c0d75ff89
yugo <gordienko.yuri@gmail.com>
2023-11-13 20:02:49 +0200

    updated

--- hw_2_base_operations/src/main/java/ua/com/alevel/Menu.java
+++ hw_2_base_operations/src/main/java/ua/com/alevel/Menu.java
@@ -15,6 +15,7 @@ public class Menu {
             variants(bf, select);
         }
     }
+
     private void menu() {
         System.out.println("_________________________________________________");
         System.out.println("- 'Sum of numbers', please enter  1");
@@ -22,6 +23,7 @@ public class Menu {
         System.out.println("- 'Lessons', please enter         3");
         System.out.println("- 'EXIT', please enter            0");
     }
+
     private void variants(BufferedReader reader, String select) throws IOException {
         switch (select) {
             case "1" -> numbers(reader);
@@ -31,28 +33,32 @@ public class Menu {
         }
         menu();
     }
+
     private void numbers(BufferedReader bf) throws IOException {
         System.out.println("You can Enter different symbols and get their sum:");
         Numbers num = new Numbers();
         System.out.println("symbol's sum is:" + " " + num.getNumbers(bf.readLine()));
     }
+
     private void words(BufferedReader bf) throws IOException {
         System.out.println("You can Enter different symbols and get its in order alphabet with quantity of each other:");
-        String l = bf.readLine();
-        char[] letters = l.toCharArray();
+        String inputstring = bf.readLine();
+        char[] letters = inputstring.toCharArray();
         Arrays.sort(letters);
         String line = String.copyValueOf(letters);
         for (int i = 0; i < letters.length; i++) {
             if (Character.isLetter(letters[i])) {
-                System.out.println(letters[i] + " - " + (line.lastIndexOf(letters[i]) - line.indexOf(letters[i]) + 1));
+                System.out.println(letters[i] + " - " +
+                        (line.lastIndexOf(letters[i]) -
+                         line.indexOf(letters[i]) + 1));
                 i += (line.lastIndexOf(letters[i]) - line.indexOf(letters[i]));
             }
         }
     }
+
     private void lessons(BufferedReader bf) throws IOException {
         System.out.println("You can Enter the lesson's number from 1 - 10, which you want to know about end:");
         School el = new School();
         el.lesson(Integer.parseInt(bf.readLine()));
     }
-
 }
--- hw_2_base_operations/src/main/java/ua/com/alevel/Numbers.java
+++ hw_2_base_operations/src/main/java/ua/com/alevel/Numbers.java
@@ -2,8 +2,8 @@ package ua.com.alevel;
 
 public class Numbers {
 
-    public int getNumbers(String count) {
-        char[] charsNum = count.toCharArray();            // создаю из строки массив чаров
+    public int getNumbers(String string) {
+        char[] charsNum = string.toCharArray();           // создаю из строки массив чаров
         int sum = 0;
         for (char c : charsNum) {                         // пробежался по массиву
             if (c <= 57 && c >= 48) {                     // если в введенной строке были цифры согласно диапазону (ascii),
@@ -11,6 +11,23 @@ public class Numbers {
             }
         }
         return (sum);
-    }
 
+
+//        System.out.println(string);
+//
+//        int sum = 0;
+//        char[] chars = string.toCharArray();
+//
+//        for (int i = 48; i <= 57; i++) {
+////            System.out.println("index: " + i + "   symbol: " + (char) i);
+//            for (int i1 = 0; i1 < chars.length; i1++) {
+////                System.out.println(chars[i1]);
+//                if ((char) i == chars[i1]) {
+//                    sum += Character.getNumericValue(chars[i1]);
+//                }
+//            }
+//        }
+//        System.out.println(sum);
+//        return sum;
+    }
 }
