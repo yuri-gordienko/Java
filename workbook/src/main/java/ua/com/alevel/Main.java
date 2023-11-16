aaf4129c6d051f1826e41b5b407459196bd9bc7e
yugo <gordienko.yuri@gmail.com>
2023-11-16 15:48:27 +0200

    main

--- workbook/src/main/java/ua/com/alevel/Main.java
+++ workbook/src/main/java/ua/com/alevel/Main.java
@@ -1,24 +1,13 @@
 package ua.com.alevel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List list = new ArrayList<>();
-        list.add(00.23);
-        System.out.println(list);
-
-//lmkkk
-//        System.out.println(s);
-//        String s =
-//        Object Object obj = list.get(0);
-
     }
+
+
 
 
 }
