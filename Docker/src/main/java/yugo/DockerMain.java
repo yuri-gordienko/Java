3ee09ce61531e1335921731b31dbc265dcf8bbd9
yugo <gordienko.yuri@gmail.com>
2023-11-30 13:15:13 +0200

    Docker

--- Docker/src/main/java/yugo/DockerMain.java
+++ Docker/src/main/java/yugo/DockerMain.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerMain {
+
     public static void main(String[] args) {
 
         SpringApplication.run(DockerMain.class, args);
