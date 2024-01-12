1356c8c36d99f104db66461e3d47d1be00f03a81
yuri-gordienko <gordienko.yuri@gmail.com>
2024-01-12 20:55:57 +0200

    sql

--- unit_11_my_sql/src/main/resources/data.sql
+++ unit_11_my_sql/src/main/resources/data.sql
@@ -3,21 +3,21 @@ insert into employees values (4, 'Павло', 'Біліневич', 25);
 insert into employees values (5, 'Юрій', 'Гордієнко', 40);
 insert into employees values (6, 'Вадим', 'Єфіменко', 26);
 
-insert into dep_emp values (1, 5)
-insert into dep_emp values (1, 6);
-insert into dep_emp values (2, 9);
+insert into dep_emp values (4, 13)
+insert into dep_emp values (1, 11);
+insert into dep_emp values (3, 12);
 
 # update
 update employees set age = 19 where id = 1;
+update dep_emp set dep_id = 2 where emp_id = 10;
 
 # delete
-delete from employees where id = 1;
+delete from employees where id = 21;
 delete from employees where id in (10, 8);  -- конкретні дані (cases)
 --
 delete from employees where id = 1; -- якщо видаляємо працівника, то
-delete from dep_emp where ud = 1; -- треба і видалити з реляційної таблиці
+delete from dep_emp where id = 1; -- треба і видалити з реляційної таблиці
 --
-
 
 # read
 select * from employees;
@@ -37,3 +37,21 @@ select * from employees where upper(first_name) = 'EVA';
 select * from employees where first_name like 'Юрій' or last_name like 'Єфіменко';
 select count(*) as count_of_employee from employees where first_name like 'Юрій' or last_name like 'Єфіменко';
 
+select * from departments d join dep_emp de
+select * from departments d join dep_emp de where de.dep_id = d.id
+select name, emp_id from departments d join dep_emp de where de.dep_id = d.id
+select id, name, count(dep_id) as employee_count from departments d join dep_emp de on de.dep_id = d.id group by d.id
+order by employee_count desc
+select * from employees join dep_emp de on employees.id = de.emp_id where de.dep_id = 1;
+
+-- find all emp in java
+select id, first_name, last_name from employees join dep_emp de on employees.id = de.emp_id where de.dep_id = 1;
+-- find all emp not in java, наприклад звіт для потенційних клієнтів
+select id, first_name, last_name, age from employees where id not in (
+            select id from employees left join dep_emp de on employees.id = de.emp_id where de.dep_id = 1
+)
+
+-- just java
+select id from employees left join dep_emp de on employees.id = de.emp_id where de.dep_id = 1
+-- not in java
+select id, first_name, last_name, age from employees where id not in (5, 6, 8, 9, 11);
