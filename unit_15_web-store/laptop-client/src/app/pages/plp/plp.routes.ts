b3a270b8b6c971b95991e98e4bdcf2db9a2b70a7
yuri-gordienko <gordienko.yuri@gmail.com>
2024-02-20 15:21:27 +0200

    plp

--- unit_15_web-store/laptop-client/src/app/pages/plp/plp.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from "@angular/router";
-import {PlpComponent} from "./plp.component";
-
-export const PLP_ROUTES: Routes = [
-  {
-    path: '',
-    component: PlpComponent
-  }
-]
