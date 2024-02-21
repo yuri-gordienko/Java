2b536e5e90f55ad9cfe635b4df2e3ad0a867cd02
yuri-gordienko <gordienko.yuri@gmail.com>
2024-02-21 09:07:54 +0200

    hibernate

--- unit_14_hibernate/src/main/java/yugo/entity/Department.java
+++ unit_14_hibernate/src/main/java/yugo/entity/Department.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "departments")
+@Table(name = "departments") // а вот без этой аннотации обойтись можно, по дефолту Hibernate будет называть именем джава класса
 public class Department extends BaseEntity {
 
     private String name;
--- unit_14_hibernate/src/main/java/yugo/entity/Employee.java
+++ unit_14_hibernate/src/main/java/yugo/entity/Employee.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "employees")
+@Table(name = "employees") // а вот без этой аннотации обойтись можно, по дефолту Hibernate будет называть именем джава класса
 public class Employee extends BaseEntity {
 
     @Column(name = "first_name")
