56fa19117a16a60753201f1f86324a93ae81b358
yuri-gordienko <gordienko.yuri@gmail.com>
2024-03-04 10:21:57 +0200

    GenerateId variants

--- work_book_1.1/src/main/java/yugo/CRUD/GenerateId.java
+++ work_book_1.1/src/main/java/yugo/CRUD/GenerateId.java
@@ -1,16 +1,37 @@
 package yugo.CRUD;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.UUID;
 
 public class GenerateId {
 
-    public static String generateId(List<Object> objectsStrings) {
+    public static String generateId(Object[] objects) {
         String id = UUID.randomUUID().toString();
 
-        if (objectsStrings.stream().anyMatch(object -> object != null && id.equals(object.getId()))) {
-            return generateId(objectsStrings);
+//        for (Object object : objectsStrings) {
+//            if (object != null && object.getId().equals(id)) {
+//                return generateId(objectsStrings);
+//            }
+//        }
+//        System.out.println("\nlistIds = " + id);
+//        return id;
+//        ---------------------------------------------------
+//        if (Arrays.stream(objectsStrings).anyMatch(object -> object.getId().equals(id) ?
+//                generateId(objectsStrings) | return id;));
+
+//        return Arrays.stream(objects).anyMatch(object -> object.getId().equals(id))
+//                ? generateId(objects)
+//                : id;
+//        ------------------------------------------
+
+        if (Arrays.stream(objects).anyMatch(object -> object.getId().equals(id))) {
+            return generateId(objects); // Якщо умова виконується, рекурсивно викликаємо метод
         }
+        System.out.println("\nlistIds = " + id);
         return id;
+
+//        if (objectsStrings.stream().anyMatch(object -> object.getId().equals(id))) {
+//            return generateId(objectsStrings);
+//        }
     }
 }
