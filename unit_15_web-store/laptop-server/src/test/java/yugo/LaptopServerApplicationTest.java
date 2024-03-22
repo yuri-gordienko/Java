dcbd54e1f32940feaebd142d796d36b0e72cd4b5
yugo <gordienko.yuri@gmail.com>
2024-03-22 18:02:51 +0200

    tests

---
+++ unit_15_web-store/laptop-server/src/test/java/yugo/LaptopServerApplicationTest.java
@@ -0,0 +1,11 @@
+package yugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LaptopServerApplicationTest {
+
+    @Test
+    void contextLoads() { }
+}
---
+++ unit_15_web-store/laptop-server/src/test/java/yugo/unit/service/ProductCrudServiceTest.java
@@ -0,0 +1,43 @@
+package yugo.unit.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import yugo.persistence.sql.entity.product.Product;
+import yugo.persistence.sql.repository.product.ProductRepository;
+import yugo.service.crud.product.impl.ProductCrudServiceImpl;
+import yugo.util.types_of_laptops.ProductBrandType;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class ProductCrudServiceTest {
+
+    private Product product = new Product();
+    private final String PRODUCT_NAME = "MacBook";
+
+
+    @InjectMocks
+    private ProductCrudServiceImpl service;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @Test
+    public void ProductShouldBeCreateWhenFieldsIsCorrect() {
+//        given
+        product.setName(PRODUCT_NAME);
+        product.setProductBrand(ProductBrandType.APPLE);
+
+//        when
+        service.create(product);
+
+//        then
+        verify(productRepository, times(1)).save(product);
+//        verify(productRepository, never()).save(product);
+
+    }
+}
