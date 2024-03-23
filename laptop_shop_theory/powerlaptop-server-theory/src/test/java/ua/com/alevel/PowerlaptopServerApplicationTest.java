d579045a1906c9d7f118843ae840e315ea9c16a5
yuri-gordienko <gordienko.yuri@gmail.com>
2024-03-23 10:34:29 +0200

    tests

--- laptop_shop_theory/powerlaptop-server-theory/src/test/java/ua/com/alevel/PowerlaptopServerApplicationTest.java
+++ laptop_shop_theory/powerlaptop-server-theory/src/test/java/ua/com/alevel/PowerlaptopServerApplicationTest.java
@@ -3,7 +3,7 @@ package ua.com.alevel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest // корневой тест-класс, создается автоматически при генерации приложения через Springititializer
+@SpringBootTest // корневой тест-класс, создается автоматически при генерации приложения через SpringInitializer
 public class PowerlaptopServerApplicationTest {
 
     @Test   // тест - заглушка, проверка что тесты запускаются, точка входа в систему (как SpringBootApplication.run)
