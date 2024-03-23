b7a77370646c65517d90dd1a645cb4b34a30b7ad
yuri-gordienko <gordienko.yuri@gmail.com>
2024-03-23 10:27:38 +0200

    tests

--- laptop_shop_theory/powerlaptop-server-theory/src/test/java/ua/com/alevel/PowerlaptopServerApplicationTest.java
+++ laptop_shop_theory/powerlaptop-server-theory/src/test/java/ua/com/alevel/PowerlaptopServerApplicationTest.java
@@ -6,6 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest // корневой тест-класс, создается автоматически при генерации приложения через Springititializer
 public class PowerlaptopServerApplicationTest {
 
-    @Test   // тест - заглушка, проверка что тесты запускаются, точка входа в систему (як SpringBootApplication.run)
+    @Test   // тест - заглушка, проверка что тесты запускаются, точка входа в систему (как SpringBootApplication.run)
     void contextLoads() { }
 }
