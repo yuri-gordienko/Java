d69d4073b4038bf1e609cc8e79a5e9eed59ee715
yuri-gordienko <gordienko.yuri@gmail.com>
2024-03-28 13:01:48 +0200

    tests

--- work_book_1.1/src/main/java/yugo/Main.java
+++ work_book_1.1/src/main/java/yugo/Main.java
@@ -3,35 +3,6 @@ package yugo;
 public class Main {
     public static void main(String[] args) {
 
-//        int[] b = new int[] {1,2,3};
-
-//        for (int i = 10; i < 20; i+= 2) {
-//            if (i > 15)
-//                break;
-//            System.out.println("i = " + i);
-//            if (i % 4 == 0)
-//                continue;
-//            System.out.println("if i % 4 = " + i);
-//        }
-
-//        int v = 10;
-//        switch (v) {
-//            case 0: System.out.println("0");
-//            case 1: System.out.println("1"); break;
-//            case 2: System.out.println("2");
-//            case 3: System.out.println("3"); break;
-//            default: System.out.println("!");
-//        }
-
-//        int v = 2;
-//        switch (v) {
-//            case 0 -> System.out.println("0");
-//            case 1 -> System.out.println("1");
-//            case 2 -> System.out.println("2");
-//            case 3 -> System.out.println("3");
-//            default -> System.out.println("!");
-//        }
-
 
     }
 }
