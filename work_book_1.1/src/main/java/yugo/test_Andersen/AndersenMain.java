7a27c45d6f0e6f37040cc35819802ae200160031
yugo <gordienko.yuri@gmail.com>
2024-08-17 10:03:10 +0300

    test

--- work_book_1.1/src/main/java/yugo/test_Andersen/AndersenMain.java
+++ work_book_1.1/src/main/java/yugo/test_Andersen/AndersenMain.java
@@ -9,18 +9,23 @@ public class AndersenMain {
 //        changeInteger.change(value);
 //        System.out.println(value);
 
-//        ThreadsDemo1 t1 = new ThreadsDemo1();
-//        ThreadsDemo2 t2 = new ThreadsDemo2();
-//        t1.start();
-//        t2.start();
 
 //        Thread t = new Test2() {
+
 //            public void run() {
 //                System.out.println(" foo");
 //            }
 //        };
 //        t.start();
 
-        new Thread(new Test3()).start();
+
+//        new Thread(new Test3()).start();
+
+
+        ThreadsDemo1 t1 = new ThreadsDemo1();
+        ThreadsDemo2 t2 = new ThreadsDemo2();
+        t1.start();
+        t2.start();
+
     }
 }
--- work_book_1.1/src/main/java/yugo/test_Andersen/ThreadsDemo2.java
+++ work_book_1.1/src/main/java/yugo/test_Andersen/ThreadsDemo2.java
@@ -1,6 +1,7 @@
 package yugo.test_Andersen;
 
 public class ThreadsDemo2 extends Thread {
+
     public void run() {
         for (int i = 0; i < 4; i++) {
             System.out.println("C");
