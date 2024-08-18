33946be421d785b56a81e5e2045de88a1417197b
yugo <gordienko.yuri@gmail.com>
2024-08-18 13:42:41 +0300

    test

--- work_book_1.1/src/main/java/yugo/Main.java
+++ work_book_1.1/src/main/java/yugo/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello again !!! ");
+        int a = 5, b = 3;
+        System.out.println("a = " + a + b);
+
 
     }
 
