fa60bc684b62212c81cd5972f79fcf544db24d59
yugo <gordienko.yuri@gmail.com>
2024-08-18 13:41:56 +0300

    test

--- work_book_1.1/src/main/java/yugo/test_ProfITsoft/ProfITSoftMain.java
+++ work_book_1.1/src/main/java/yugo/test_ProfITsoft/ProfITSoftMain.java
@@ -3,7 +3,20 @@ package yugo.test_ProfITsoft;
 public class ProfITSoftMain {
     public static void main(String[] args) {
 
+//        bool a = false;
+//        byte b = 10000;
+//        float c = 13.1f;
+//        char d = 'ab';
+//        int[] e;
+
+
+//        int[] a = new int {1,2,3};
 //        int[] b = new int[] {1,2,3};
+//        int[] c = int[] {1,2,3};
+//        int d[] = 1,2,3;
+//        int e = new int[] {1,2,3};
+//        int[] f = {4,5,6};
+
 
 //        for (int i = 10; i < 20; i+= 2) {
 //            if (i > 15)
@@ -14,7 +27,7 @@ public class ProfITSoftMain {
 //            System.out.println("if i % 4 = " + i);
 //        }
 
-//        int v = 10;
+//        int v = 4;
 //        switch (v) {
 //            case 0: System.out.println("0");
 //            case 1: System.out.println("1"); break;
