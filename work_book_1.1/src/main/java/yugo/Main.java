c6e53bd3d323744b59fd23a3e280d79078919c4a
yugo <gordienko.yuri@gmail.com>
2024-09-15 14:22:59 +0300

    test

--- work_book_1.1/src/main/java/yugo/Main.java
+++ work_book_1.1/src/main/java/yugo/Main.java
@@ -1,25 +1,23 @@
 package yugo;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
 //        int n = 5; // Розмір масиву
 //        int index = 0;
-//
 //        for (int i = 0; i < 10; i++) {
 //            index = i % n;
 //            System.out.println("Index: " + index);
 //        }
 
 
-
-
-
-
-
-
-
+//        String s = "Mrs. Robinson";
+//        String s2 = "Hello, ".concat(s);
+//        System.out.println("s2 = " + s2);
 
 
 
